e786be9ab4d490c794efe2e3aa710ccf1f8f40a8
Mariano <mariano.escobal@cencosud.com>
2023-06-03 10:32:52 -0300

    More Value Objects

---
+++ Negocio/ValueObjects/DescripcionTipoCabania.cs
@@ -0,0 +1,35 @@
+﻿using Negocio.ExcepcionesPropias;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Negocio.ValueObjects
+{
+    public class DescripcionTipoCabania
+    {
+        public static int CantMinCarNombre { get; set; }
+        public static int CantMaxCarNombre { get; set; }
+
+        public string Value { get; private set; }
+
+        public DescripcionTipoCabania(string Descripcion)
+        {
+            Value = Descripcion;
+            Validar();
+        }
+
+        private DescripcionTipoCabania()
+        {
+        }
+
+        public void Validar()
+        {
+            if (Value.Length > CantMaxCarNombre || Value.Length < CantMinCarNombre)
+            {
+                throw new DescripcionInvalidaException($"La descripcion no puede tener menos de {CantMinCarNombre} caracteres ni mas de {CantMaxCarNombre}");
+            }
+        }
+    }
+}
---
+++ Negocio/ValueObjects/NombreCabania.cs
@@ -0,0 +1,45 @@
+﻿using Negocio.ExcepcionesPropias.Cabanias;
+using Negocio.InterfacesDominio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Negocio.ValueObjects
+{
+    public class NombreCabania : IValidable
+    {
+
+        public string Value { get; private set; }
+
+        public NombreCabania(string nom)
+        {
+            Value = nom;
+            Validar();
+        }
+
+        private NombreCabania()
+        {
+        }
+
+        public void Validar()
+        {
+            #region Validar Nombre
+            if (string.IsNullOrEmpty(Value))
+            {
+
+                throw new NombreInvalidoException("El nombre no puede ser nulo o vacío");
+
+            }
+            if (Regex.IsMatch(Value, "^[a-zA-Z]+( [a-zA-Z]+)*$"))
+            {
+                throw new NombreInvalidoException("El nombre solo incluye caracteres alfabéticos y espacios embebidos, pero no al principio ni final");
+            }
+
+            #endregion
+        }
+    }
+}
