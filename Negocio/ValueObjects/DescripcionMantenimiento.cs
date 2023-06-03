4a4ace9be457640d6d903125e7604aa13524c189
BrunoPerez <brunoperez24793@gmail.com>
2023-06-03 10:32:37 -0300

    Value Objects

---
+++ Negocio/ValueObjects/DescripcionMantenimiento.cs
@@ -0,0 +1,32 @@
+﻿using Negocio.ExcepcionesPropias;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Negocio.ValueObjects
+{
+    public class DescripcionMantenimiento
+    {
+        public string Value { get; private set; }
+        public DescripcionMantenimiento(string descripcion)
+        {
+            Value = descripcion;
+            Validar();
+        }
+
+        private DescripcionMantenimiento() { }
+
+        public void Validar()
+        {
+
+            if (Value.Length < 10 || Value.Length > 200 || Value == null)
+            {
+                throw new CaracteresDentroDeRango("La cantidad de caracteres debe de ser entre 10 y 200");
+            }
+
+        }
+    }
+}
+
---
+++ Negocio/ValueObjects/PasswordUsuario.cs
@@ -0,0 +1,39 @@
+﻿using Negocio.ExcepcionesPropias;
+using Negocio.InterfacesDominio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Negocio.ValueObjects
+{
+    public class PasswordUsuario :IValidable
+    {
+
+        public string Value { get; private set; }
+        public PasswordUsuario(string password) 
+        { 
+            Value = password;
+            Validar();
+        }
+
+        private PasswordUsuario() { }
+
+        public void Validar()
+        {
+
+            if (string.IsNullOrEmpty(Value))
+            {
+                throw new CampoVacioException("La contraseña no puede estar vacia");
+            }
+
+            if (Value.Length < 6 && !Value.Any(char.IsUpper) == false && !Value.Any(char.IsLower) == false && !Value.Any(char.IsDigit) == false)
+            {
+                throw new ContraseñaException("La contraseña tiene que tener al menos 6 caracteres que incluyan letras mayúsculas " +
+                    "\r\n y minúsculas (al menos una de cada una) y dígitos (0 al 9)");
+            }
+
+        }
+    }
+}
