e158da58cfe065fb3dc62524361bf9a4a763ff66
BrunoPerez <brunoperez24793@gmail.com>
2023-06-03 10:11:48 -0300

    dtos

--- DTOs/MantenimientoDTO.cs
+++ DTOs/MantenimientoDTO.cs
@@ -1,12 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace DTOs
 {
-    internal class MantenimientoDTO
+    public class MantenimientoDTO
     {
+        public DateTime fecha { get; set; }
+        public string descripcion { get; set; }
+        public decimal costo { get; set; }
+        [Required]
+        public string trabajador { get; set; }
+        public CabaniaDTO cabania { get; set; }
+        public int CabaniaId { get; set; }
+        public int Id { get; set; }
     }
 }
